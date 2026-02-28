32b636ffd8a7633cc38315662b477d4aa7050e77
subairnifras <mohamednifras508@gmail.com>
2026-02-28 14:25:18 +0530

    error fixed

--- src/scripts/nodeCheck.js
+++ src/scripts/nodeCheck.js
@@ -1,2 +1,5 @@
 console.log("Node version:", process.version);
 console.log("App is running successfully inside Node 20!");
+
+// Keep container alive
+setInterval(() => {}, 1000 * 60); // 1 minute interval
